8ca95d02a3b25b0a3f0768f2466e29bcd14a2211
danascape <danascape@gmail.com>
2021-07-02 16:50:27 +0530

    X00P: Build aosp aidl power HAL

--- device.mk
+++ device.mk
@@ -301,7 +301,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
